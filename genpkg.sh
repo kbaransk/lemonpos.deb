28a216472ef7ac122ccaa5179b7cd5ed9f8aa6de
Krzysztof Barański <kbaransk@gmail.com>
2018-12-13 23:37:12 +0100

    Update for lemonpos-fast_0.9.9-1

--- genpkg.sh
+++ genpkg.sh
@@ -2,6 +2,7 @@
 
 CHECKOUTDIR=lemonpos
 WORKDIR=out
+UPSTREAM_VERSION=0.9.9
 
 rm -fr   $WORKDIR
 mkdir -p $WORKDIR
@@ -9,11 +10,14 @@ mkdir -p $WORKDIR
 
 if [ ! -d $CHECKOUTDIR ]
 then
-	#git clone https://github.com/kbaransk/lemonpos $CHECKOUTDIR
-	git clone git@github.com:kbaransk/lemonpos.git $CHECKOUTDIR
+	git clone https://github.com/kbaransk/lemonpos $CHECKOUTDIR
+	#git clone git@github.com:kbaransk/lemonpos.git $CHECKOUTDIR
+	pushd $CHECKOUTDIR
+		git checkout tags/lemonpos-fast_${UPSTREAM_VERSION}
+	popd
 fi
 
-tar -czf $WORKDIR/lemonpos-fast_0.9.8.orig.tar.gz --exclude .git --exclude .gitignore lemonpos
+tar -czf $WORKDIR/lemonpos-fast_${UPSTREAM_VERSION}.orig.tar.gz --exclude .git --exclude .gitignore lemonpos
 cp -a lemonpos $WORKDIR/lemonpos
 cp -a debian $WORKDIR/lemonpos/debian
 rm -fr $WORKDIR/lemonpos/.git
